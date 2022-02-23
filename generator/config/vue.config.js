e8406eae2ad98ac6d135d4c94bfa825e1f15d1cc
开关 <gltwork@163.com>
2022-02-23 19:01:27 +0800

    修复Bug

--- generator/config/vue.config.js
+++ generator/config/vue.config.js
@@ -1,8 +1,22 @@
+const webpack = require('webpack')
+
 module.exports = {
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config.plugin('ignore').use(new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /^\.\/(zh-cn|vi)$/i))
+  },
   css: {
     loaderOptions: {
       sass: {
         data: `@import "~@/styles/global";`
+      }
+    }
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: '',
+        changeOrigin: true
       }
     }
   }
